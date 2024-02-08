5bc15d90adc057d46f69835450d13f91a0c28345
Aaron Isaac <aaron@aisaac.co>
2024-02-07 22:58:45 -0800

    New game using libgdx

---
+++ desktop/src/co/aisaac/zombie/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package co.aisaac.zombie;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import co.aisaac.zombie.ZombieGame;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("Zombie Game");
+		new Lwjgl3Application(new ZombieGame(), config);
+	}
+}
